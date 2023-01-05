5bc1a76d3c3c41bbf3324e5521a9bae6db4e05a4
Ryan Kroon <rykroon.tech@gmail.com>
2023-01-04 20:11:17 -0500

    Refactored types to use python 3.11's new Required and NotRequired features.

--- jsonstarpc/types.py
+++ jsonstarpc/types.py
@@ -1,35 +1,26 @@
-import typing as t
+from typing import Any, Literal, NotRequired, Required, TypedDict
 
 
-class _JsonRpc(t.TypedDict):
-    jsonrpc: str
+class JsonRpcRequest(TypedDict):
+    jsonrpc: Required[Literal['2.0']]
+    method: Required[str]
+    params: NotRequired[dict[str, Any] | list[Any]]
+    id: NotRequired[str | int]
 
 
-class _JsonRpcRequestRequired(_JsonRpc):
-    method: str
+class JsonRpcSuccessResponse(TypedDict):
+    jsonrpc: Required[Literal['2.0']]
+    result: Required[Any]
+    id: Required[str | int | None]
 
 
-class JsonRpcRequest(_JsonRpcRequestRequired, total=False):
-    params: dict[str, t.Any] | list[t.Any]
-    id: str | int
+class JsonRpcError(TypedDict):
+    code: Required[int]
+    message: Required[str]
+    data: NotRequired[dict[str, Any]]
 
 
-class _JsonRpcResponse(_JsonRpc):
-    id: str | int | None
-
-
-class JsonRpcSuccessResponse(_JsonRpcResponse):
-    result: t.Any
-
-
-class _JsonRpcErrorRequired(t.TypedDict):
-    code: int
-    message: str
-
-
-class JsonRpcError(_JsonRpcErrorRequired, total=False):
-    data: dict[str, t.Any]
-
-
-class JsonRpcErrorResponse(_JsonRpcResponse):
-    error: JsonRpcError
+class JsonRpcErrorResponse(TypedDict):
+    jsonrpc: Required[Literal['2.0']]
+    error: Required[JsonRpcError]
+    id: Required[str | int | None]
